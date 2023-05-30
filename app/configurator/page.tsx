82952614f679bef590275a8baf207a8bf584b983
ramona <rbellamy@sparkcognition.com>
2023-05-29 21:06:16 -0500

    3d model

--- app/configurator/page.tsx
+++ app/configurator/page.tsx
@@ -2,14 +2,24 @@
 
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import LogoPicker from "../components/logoPicker";
+import LogoPicker from "./components/logoPicker";
 import Image from "next/image";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import {Model } from "./models/Shoe";
 
 const Page = () => {
 
   return (
-    
-     <LogoPicker/>
+    <div className="shoe">
+    <Canvas>
+        <Environment preset="sunset" />
+        <Model/>
+        <ContactShadows position={[0, -3.2, 0]} rotation={[-Math.PI / 2, 0, 0]} opacity={0.25} width={10} height={10} blur={1} far={9} />
+        <OrbitControls autoRotate />
+    </Canvas>
+    </div>
+
  
   );
 };
